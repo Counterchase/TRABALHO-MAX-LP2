7645c713436620239100256a2650e73c721af777
Counterchase <IFMS@BRJ60208T2.academico.local>
2018-10-30 23:24:54 -0300

    A principio esta Pronto

--- src/projeto/Teste.java
+++ src/projeto/Teste.java
@@ -1,37 +1,60 @@
 package projeto;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 
 public class Teste {
 
+    Color cor1 = Color.BLACK;
+    Color cor2 = Color.BLUE;
+
+    JButton b1 = new JButton("Mudar Cor 1");
+    JButton b2 = new JButton("Mudar Cor 2");
+    JFrame frame = new JFrame();
+    JGradientPanel painel = new JGradientPanel(cor1, cor2);
+
     public Teste() {
-        
-        JFrame frame = new JFrame();
-        JGradientPanel painel = new JGradientPanel(Color.WHITE, Color.BLUE);
+
         frame.setContentPane(painel);
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
 
-        JButton b1 = new JButton("Mudar Cor 1");
         frame.add(b1);
         b1.setBounds(500, 340, 100, 20);
 
-        JButton b2 = new JButton("Mudar Cor 2");
         frame.add(b2);
         b1.setBounds(500, 500, 100, 20);
-       
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                cor1 =  JColorChooser.showDialog(frame, "escolha sua cor", cor1);              
+                painel.setInitialColor(cor1);
+                painel.repaint();
+                
+            }
+        });
+        
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                cor2 =  JColorChooser.showDialog(null, "escolha sua cor", cor2);
+                 painel.setFinalColor(cor2);
+                 painel.repaint();
+            }
+        });
+
     }
-    
-    
-    
-    
 
     public static void main(String[] args) {
-        
+
         Teste a = new Teste();
     }
 }
